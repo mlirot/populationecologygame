ccfebbf0b2ed1da7ee9ce4bda2b5be7acf4e065b
mlirot <mlirot@gatewayoh.org>
2025-09-20 15:53:41 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/populationecologygame/',  // 👈 add this line
+  base: '/populationecologygame/',  // 👈 important for GitHub Pages
 })
